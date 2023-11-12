b987887d3a4fe18a7544f3d2751f735b04d6ec43
Arne Bahlo <hey@arne.me>
2023-11-12 15:11:27 +0100

    feat(server): Add basic content

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use axum::{routing::get, Router};
-use maud::{html, Markup};
+use maud::{html, Markup, PreEscaped, DOCTYPE};
 use std::net::SocketAddr;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+const FIRE_CHICKEN: &str = include_str!("../static/fire-chicken.svg");
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -25,6 +27,41 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
 async fn index() -> Markup {
     html! {
-        h1 { "Fire Chicken Webring" }
+        (DOCTYPE)
+        html lang="en" {
+            head {
+                title { "Fire Chicken Webring" }
+                meta charset="utf-8";
+                meta name="title" content="Fire Chicken Webring";
+                meta name="description" content="An invite-only webring for personal websites.";
+                meta name="author" content="Arne Bahlo";
+                meta name="theme-color" content="color(srgb 0.9429 0.3521 0.1599)";
+                meta name="viewport" content="width=device-width,initial-scale=1";
+                meta property="og:type" content="website";
+                meta property="og:url" content="firechicken.club";
+                meta property="og:title" content="Fire Chicken Webring";
+                meta property="og:description" content="An invite-only webring for personal websites.";
+                style { (PreEscaped(include_str!("../static/style.css"))) }
+            }
+            body {
+                .sitewrapper {
+                    h1 { "Fire Chicken Webring" }
+                    p { "An invite-only webring for personal websites." }
+                    main {
+                        section.members {
+                            h2 { "Members" }
+                            ul {
+                                li {
+                                    a href="https://arne.me" { "Arne Bahlo" }
+                                }
+                            }
+                        }
+                        section.fire_chicken {
+                            (PreEscaped(FIRE_CHICKEN))
+                        }
+                    }
+                }
+            }
+        }
     }
 }
