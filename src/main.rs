8b38a4dc817b08765626453e89110384129be2d2
Arne Bahlo <hey@arne.me>
2023-11-12 14:48:22 +0100

    feat(server): Add tracing, tracing-subscriber

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    tracing_subscriber::fmt::init();
+
     let app = Router::new().route("/", get(index));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("Listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await?;
