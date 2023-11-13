1c7e51053d4bee78d7035b917280a75ec968425d
Arne Bahlo <hey@arne.me>
2023-11-13 09:33:17 +0100

    fix(generator): Add download-fonts, clap

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use anyhow::{anyhow, bail, Result};
 use chrono::NaiveDate;
+use clap::Parser;
 use maud::{html, Markup, PreEscaped, DOCTYPE};
 use rand::seq::SliceRandom;
 use serde::Deserialize;
-use std::{fs, path::Path};
+use std::{
+    env,
+    fs::{self, File},
+    io,
+    path::Path,
+};
+use tempdir::TempDir;
 use url::Url;
+use zip::ZipArchive;
 
 #[derive(Debug, Deserialize)]
 struct FireChicken {
@@ -50,7 +58,30 @@ struct Member {
     joined: NaiveDate,
 }
 
+#[derive(Debug, Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Parser)]
+enum Commands {
+    #[clap(name = "build")]
+    Build,
+    #[clap(name = "download-fonts")]
+    DownloadFonts,
+}
+
 fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Build => build(),
+        Commands::DownloadFonts => download_fonts(),
+    }
+}
+
+fn build() -> Result<()> {
     let fire_chicken: FireChicken =
         toml::from_str(include_str!("../firechicken.toml")).expect("Invalid TOML");
 
@@ -191,5 +222,45 @@ where
         }
     }
 
+    Ok(())
+}
+
+fn download_fonts() -> Result<()> {
+    let zip_url = env::var("FONT_ZIP_URL")?;
+    let destination = Path::new("./static/fonts");
+
+    let response = ureq::get(&zip_url).call()?;
+    let mut reader = response.into_reader();
+
+    let temp_dir = TempDir::new("firechicken-club-fonts")?;
+    let zip_path = temp_dir.path().join("fonts.zip");
+    let mut temp_file = File::create(&zip_path)?;
+    io::copy(&mut reader, &mut temp_file)?;
+
+    let zip_file = File::open(&zip_path)?;
+    let mut archive = ZipArchive::new(zip_file)?;
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        let outpath = match file.enclosed_name() {
+            Some(path) => destination.join(path),
+            None => continue,
+        };
+
+        if file.name().ends_with('/') {
+            fs::create_dir_all(&outpath)?;
+        } else {
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(&p)?;
+                }
+            }
+
+            let mut outfile = File::create(&outpath)?;
+            io::copy(&mut file, &mut outfile)?;
+        }
+    }
+
+    temp_dir.close()?;
     Ok(())
 }
